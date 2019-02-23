28cb4c57d7adb058fbc9e38f0aa63db10aac0c0f
Michael Lee <mikecovlee@163.com>
2019-02-23 21:22:25 +0800

    Support new version

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 @mkdir build
 @cd build
 @mkdir imports
-@g++ -std=c++11 -I%CS_DEV_PATH%\include -I..\include -shared -static -fPIC -s -O3 ..\codec.cpp -L%CS_DEV_PATH%\lib -lcovscript -o .\imports\codec.cse
+@g++ -std=c++14 -I%CS_DEV_PATH%\include -I..\include -shared -static -fPIC -s -O3 ..\codec.cpp -L%CS_DEV_PATH%\lib -lcovscript -o .\imports\codec.cse
